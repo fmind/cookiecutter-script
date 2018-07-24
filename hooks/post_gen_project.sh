9971ccd30e3e7680c934ec2fb3c84a9a74c2e887
fmind <dev@fmind.me>
2018-07-24 17:25:19 +0200

    added exit script

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-chmod u+x {{cookiecutter.name}}.py
-mv {{cookiecutter.name}}.py ../
+chmod u+x '{{cookiecutter.name}}.py'
+mv '{{cookiecutter.name}}.py' ../
 
-cd ..; rmdir {{cookiecutter.name}}
+cd ..; rmdir '{{cookiecutter.name}}'
--- {{cookiecutter.name}}/{{cookiecutter.name}}.py
+++ {{cookiecutter.name}}/{{cookiecutter.name}}.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Documentation of the program."""
 
+import sys
 import logging
 import argparse
 
@@ -23,6 +24,8 @@ def main(args=None):
     for line in opts.input:
         print(reverse(line.rstrip()))
 
+    return 0
+
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
